VoidUI/vendor_aosp

permissions: Enable Google LILY_EXPERIENCE feature
* Extracted from Google Camera Go official release 3.6.455515391
* Checked from Google Camera Go, Gallery Go
* It enables several Android Go specific features for Android Go Apps

Example:
Google Camera Go:
* HDR, Night Mode, Snapchat Sticker integration

Signed-off-by: someone5678 <nemui3353@gmail.com>
Change-Id: I3055639b9afe9189666d58557dfac0b30f298f76

config/common.mk
@@ -60,6 +60,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/base/data/keyboards/Vendor_045e_Product_028e.kl:$(TARGET_COPY_OUT_PRODUCT)/usr/keylayout/Vendor_045e_Product_0719.kl
 
+# Enable Google LILY_EXPERIENCE feature
+PRODUCT_COPY_FILES += \
+    vendor/aosp/target/config/permissions/lily_experience.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/lily_experience.xml
+
 # Enforce privapp-permissions whitelist
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.control_privapp_permissions=enforce

AOSP-Gumiho/vendor_gumiho

Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

config/common.mk
@@ -74,6 +74,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/lineage/config/permissions/org.lineageos.android.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/org.lineageos.android.xml
 
+# Enable Google LILY_EXPERIENCE feature
+PRODUCT_COPY_FILES += \
+    vendor/lineage/target/config/permissions/lily_experience.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/lily_experience.xml
+
 # Enforce privapp-permissions whitelist
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.control_privapp_permissions=enforce