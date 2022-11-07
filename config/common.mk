9662da0e158d1f2cf32f7322284746ed468a49c8
putraaxyzo <michelsmithe2@gmail.com>
2022-11-07 11:18:36 +0000

    vendor: build with GameSpace

--- config/common.mk
+++ config/common.mk
@@ -121,6 +121,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/lineage/prebuilt/common/etc/init/init.lineage-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.lineage-updater.rc
 
+# Gumiho Packages
+PRODUCT_PACKAGES += \
+    GameSpace
+
 # Themes
 PRODUCT_PACKAGES += \
     LineageBlackTheme \
